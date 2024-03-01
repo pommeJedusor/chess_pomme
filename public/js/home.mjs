4494605f8c54670d9f33ca96ab03722334dbe76e
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-03-01 11:59:37 +0100

    add stockfish launch game popup

--- public/js/home.mjs
+++ public/js/home.mjs
@@ -21,9 +21,43 @@ async function update_games(){
     }
 }
 
+function launch_stockfish_game(){
+    const bot_level_input = document.querySelector("#bot-level");
+    if (!bot_level_input)return;
+    const bot_level = bot_level_input.value;
+    const url = "./stockfish?level="+bot_level;
+    location.href = url;
+}
+
+function stockfish_popup(){
+    const popup = `
+    <div id="bot-parameters">
+        <h2>Stockfish</h2>
+        <form>
+            <p>Level : <span id="level-bot">10</span></p>
+            <label>Choix du level</label>
+            <input type="range" name="" id="bot-level" min="0" max="20" oninput="document.querySelector('#level-bot').textContent=document.querySelector('#bot-level').value">
+        </form>
+        <button id="stockfish-games-button">Lancer la partie</button>
+        <button id="close-button"></button>
+    </div>
+    `;
+    document.body.insertAdjacentHTML("afterbegin", popup);
+    const stockfish_button = document.querySelector("#stockfish-games-button");
+    stockfish_button.addEventListener("click", launch_stockfish_game);
+    const close_button = document.querySelector("#close-button");
+    close_button.addEventListener("click", ()=>document.querySelector("#bot-parameters").remove());
+}
+
 function main(){
     update_games();
     setInterval(update_games, 5000);
+    const link_stockfish = document.querySelector("#stockfish-games > h3 > a:first-child");
+    console.log(link_stockfish);
+    link_stockfish.addEventListener("click", (event)=>{
+        event.preventDefault();
+        stockfish_popup();
+    });
 }
 
 main();
