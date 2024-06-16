2be377cef33f59331106556210ce8dfca88888c7
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-06-16 10:24:54 +0200

    update db

--- model/db.sql
+++ model/db.sql
@@ -2,13 +2,15 @@ CREATE DATABASE IF NOT EXISTS `chess_pomme`;
 USE `chess_pomme`;
 
 CREATE TABLE IF NOT EXISTS `chess_game` (
-    `id` SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    `white_player` SMALLINT UNSIGNED,
-    `black_player` SMALLINT UNSIGNED,
-    `pgn` TEXT,
-    `winner` ENUM('white', 'black', 'draw', 'still playing') NOT NULL,
-    `date` TIMESTAMP DEFAULT NOW() NOT NULL,
-    `status` ENUM('checkmate', "stalemate", 'timeout', 'resign', 'by quit', 'mutual agreement', 'unsificient material', 'unsificient material and timeout')
+  `id` SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `white_player` SMALLINT UNSIGNED,
+  `black_player` SMALLINT UNSIGNED,
+  `white_elo` SMALLINT UNSIGNED DEFAULT NULL,
+  `black_elo` SMALLINT UNSIGNED DEFAULT NULL,
+  `pgn` TEXT,
+  `winner` ENUM('white', 'black', 'draw', 'still playing') NOT NULL,
+  `date` TIMESTAMP DEFAULT NOW() NOT NULL,
+  `status` ENUM('checkmate', "stalemate", 'timeout', 'resign', 'by quit', 'mutual agreement', 'unsificient material', 'unsificient material and timeout')
 );
 
 CREATE TABLE IF NOT EXISTS `user` (
@@ -19,8 +21,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 );
 
 CREATE TABLE IF NOT EXISTS `auth_cookie` (
-    `id` SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    `user_id` SMALLINT UNSIGNED NOT NULL,
-    `cookie` VARCHAR(255) NOT NULL UNIQUE,
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+  `id` SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `user_id` SMALLINT UNSIGNED NOT NULL,
+  `cookie` VARCHAR(255) NOT NULL UNIQUE,
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 );
